1f6d88ef7b4ba9a5347defd27c923cd53d4d2f3b
Bianca Lorenzini <biancalorenzini.dev@gmail.com>
2024-10-14 16:36:35 -0300

    :sparkles: feat: delete and update user controller file

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,10 +1,10 @@
 import { Request, Response } from "express";
 
-import { createUserService, findAllUsersService } from "../services/user.service";
+import { createUserService, deleteUserService, findAllUsersService, updateUserService } from "../services/user.service";
 
 export const createUser = async (req: Request, res: Response) => {
     try {
-        const user = createUserService(req.body)
+        const user = await createUserService(req.body)
         res.status(201).json(user)
     } catch (error) {
         res.status(400).json({message : error})
@@ -12,7 +12,27 @@ export const createUser = async (req: Request, res: Response) => {
 }
 
 export const findAllUsers = async (req: Request, res: Response) =>  {
-    const users = findAllUsersService()
+    const users = await findAllUsersService()
     res.status(200).json(users)
 }
+
+export const updateUser = async (req: Request, res: Response) => {
+    try{
+        const user = await updateUserService(Number(req.params.id), req.body)
+        res.status(200).json(user)
+    } catch(error){
+        res.status(400).json({message : {error}})
+    }
+}
+
+export const deleteUser = async (req: Request, res: Response) => {
+    try{
+       await deleteUserService(Number(req.params.id))
+        res.status(204).send()
+    } catch(error){
+        res.status(400).json({message : {error}})
+    }
+}
+
+
 
