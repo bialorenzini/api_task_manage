e231d734a493bdc189913117ff1d2261796e06b5
Bianca Lorenzini <biancalorenzini.dev@gmail.com>
2024-10-14 16:32:58 -0300

    :sparkles: feat: delete and update user service file

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,6 @@
 import { error } from "console";
 import { UserInterface } from "../interfaces/user";
-import { createUser, findAllUsers, findUserByEmail } from "../repositories/user.repository";
+import { createUser, deleteUser, findAllUsers, findUserByEmail, findUserById, updateUser } from "../repositories/user.repository";
 
 export const createUserService = async (data : UserInterface) => {
     const user = await findUserByEmail(data.email)
@@ -18,4 +18,28 @@ export const findAllUsersService = async() => {
 
 export const findUserByEmailService = async (email : string) => {
     return findUserByEmail(email)
+}
+
+export const findUserByIdService = async (id:number) => {
+    return findUserById(id)
+}
+
+export const updateUserService = async(id:number, data: UserInterface) => {
+    const user = findUserById(id)
+
+    if(!user){
+        throw new Error('Usuário não encontrado')
+    }
+
+    return updateUser(id, data)
+}
+
+export const deleteUserService = async (id:number) => {
+    const user = findUserById(id)
+
+    if(!user){
+        throw new Error('Usuário não encontrado')
+    } 
+    
+    return deleteUser(id)
 }
